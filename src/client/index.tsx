d3136017b168fd6273cdfeba517fe2fa9de48b7f
Satish P <palakoti1244@hotmail.com>
2022-01-26 22:23:55 -0500

    Fixing the webpack configurations

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,8 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 const App = ()=>(<h1>
-        My react and Typescript App!
+        My react and Typescript App
     </h1>
 )
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App/>, document.getElementById("root"))
